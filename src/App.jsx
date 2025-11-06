8e13cb601cda741d47f6d19dd24d5db9c683c2c1
DalipTandon <daliptandon8@gmail.com>
2025-11-07 00:42:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React ,{ useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-400'>
+        Init
+        </div>
+    </>
+  )
+}
+
+export default App
